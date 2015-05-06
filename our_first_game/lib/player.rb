8d220a806df28b606ab1a279fb225256bc89d19f
jsrn <james.srn@gmail.com>
2015-05-06 23:42:23 +0100

    added x and y coord to player

--- our_first_game/lib/player.rb
+++ our_first_game/lib/player.rb
@@ -1,9 +1,11 @@
 class Player
   attr_accessor :hit_points, :attack_power
+  attr_accessor :x_coord, :y_coord
 
   def initialize
-    @hit_points   = 100
-    @attack_power = 1
+    @hit_points        = 100
+    @attack_power      = 1
+    @x_coord, @y_coord = 0, 0
   end
 
   def alive?
