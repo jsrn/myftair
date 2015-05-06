b63bcc8a179bb205b042f6b6793c790ff93124f8
jsrn <james.srn@gmail.com>
2015-05-06 23:58:42 +0100

    switch for processing input

--- our_first_game/game.rb
+++ our_first_game/game.rb
@@ -11,16 +11,33 @@ class Game
   private
   def start_game
     while @player.alive?
-      print "What's the plan, Stan? "
-      action = gets.chomp.to_sym
+      action = take_player_input
       next unless allowed_actions.include? action
-      puts "Valid"
-      # take action
+
+      take_action(action)
     end
+  end
+
+  def take_player_input
+    print "What's the plan, Stan? "
+    gets.chomp.to_sym
   end
 
   def allowed_actions
     [:north, :east, :south, :west]
+  end
+
+  def take_action(action)
+    case action
+    when :north
+      move_player_north # not yet implemented
+    when :east
+      move_player_east  # not yet implemented
+    when :south
+      move_player_south # not yet implemented
+    when :west
+      move_player_west  # not yet implemented
+    end
   end
 end
 
