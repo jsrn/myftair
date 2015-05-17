a2d29169e424db6da0883f91459385648c52039d
jsrn <james.srn@gmail.com>
2015-05-17 18:23:33 +0100

    reorder some methods

--- our_first_game/lib/monster.rb
+++ our_first_game/lib/monster.rb
@@ -8,19 +8,19 @@ class Monster
     @attack_power = 1
   end
 
-  def interact(player)
-    while player.alive?
-      @hit_points -= player.attack_power
-      break unless alive?
-      player.hit_points -= @attack_power
-    end
-  end
-
   def alive?
     @hit_points > 0
   end
 
   def to_s
     "a horrible monster! garurururu"
+  end
+
+  def interact(player)
+    while player.alive?
+      @hit_points -= player.attack_power
+      break unless alive?
+      player.hit_points -= @attack_power
+    end
   end
 end
